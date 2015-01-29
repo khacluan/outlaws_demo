98524174f3e5218c94832cf9be97e5497425be25
khacluan <dangluan20@gmail.com>
2015-01-30 04:44:11 +0700

    fix cache

--- app/controllers/home_good_controller.rb
+++ app/controllers/home_good_controller.rb
@@ -2,16 +2,12 @@ class HomeGoodController < ApplicationController
   layout 'home_good'
 
   def index
-    @products = $redis.get('products')
-
-    if @products.nil?
+    @products = JSON.load $redis.get('products')
+    if @products.blank?
       @products = Product.includes(:category).pluck("products.id", "products.name", "categories.name")
       $redis.set('products', @products)
       $redis.expire('products', 3.hour.to_i)
     end
-
-    @products = JSON.load(@products) if @products.is_a?(String)
-
   end
 
   def sidebar
--- app/controllers/home_slim_controller.rb
+++ app/controllers/home_slim_controller.rb
@@ -2,16 +2,12 @@ class HomeSlimController < ApplicationController
   layout 'home_slim'
 
   def index
-    @products = $redis.get('products')
-
-    if @products.nil?
-      @products = Product.joins(:category).pluck("products.id", "products.name", "categories.name")
+    @products = JSON.load $redis.get('products')
+    if @products.blank?
+      @products = Product.includes(:category).pluck("products.id", "products.name", "categories.name")
       $redis.set('products', @products)
       $redis.expire('products', 3.hour.to_i)
     end
-
-    @products = JSON.load(@products) if @products.is_a?(String)
-
   end
 
   def sidebar
